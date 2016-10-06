7caae8f45a76c51edb8898d13c6ddbfda6e906af
Zak <zburkhardt22@gmail.com>
2016-10-06 11:12:46 -0400

    stub for vacationTracker

--- CacheBuddy/CacheBuddy/Services/VacationTracker.cs
+++ CacheBuddy/CacheBuddy/Services/VacationTracker.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,12 @@ namespace CacheBuddy.Services
 {
     public class VacationTracker
     {
+        public String getRemainingVacationDays()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\Zak\Desktop");
+            driver.Navigate().GoToUrl("https://me.jpmorganchase.com/mejpmc/");
+
+            return "0";
+        }    
     }
 }
